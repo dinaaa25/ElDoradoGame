bce94839ee075a12910ed0468bde500d56549106
Stijn Schuurman <stijn.schuurman@hotmail.com>
2024-05-11 20:17:45 +0200

    start tile and cavecoin dev with tests

---
+++ src/main/java/org/utwente/CaveCoin/CaveCoin.java
@@ -0,0 +1,11 @@
+package org.utwente.CaveCoin;
+
+public class CaveCoin {
+    private final int power;
+    private final CaveCoinType caveCoinType;
+
+    public CaveCoin(int power, CaveCoinType caveCoinType) {
+        this.power = power;
+        this.caveCoinType = caveCoinType;
+    }
+}
---
+++ src/main/java/org/utwente/Tile/TileType.java
@@ -0,0 +1,12 @@
+package org.utwente.Tile;
+
+public enum TileType {
+    Machete,
+    Paddle,
+    Coin,
+    Basecamp,
+    Discard,
+    Mountain,
+    Cave,
+    ElDorado
+}
