76ae7dfd9c5e14da88586bc637b61603f6be78e9
Fonteijn, F.F. (Finn, Student M-CS) <f.f.fonteijn@student.utwente.nl>
2024-05-15 17:54:20 +0200

    Added automated  './gradlew test' gitlab-ci.yml and included the inclusion of testcontainers into gradle requirements for (future) integration testing.

--- src/test/java/org/utwente/Tile/TestTile.java
+++ src/test/java/org/utwente/Tile/TestTile.java
@@ -1,17 +1,28 @@
 package org.utwente.Tile;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 import org.utwente.CaveCoin.CaveCoin;
 import org.utwente.CaveCoin.CaveCoinType;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@Testcontainers
 class TestTile {
+
+    @Container
+    // wont actually do anything but will show we can load external containers and
+    // test against them
+    private static final GenericContainer<?> redisContainer = new GenericContainer<>("redis:6.2.6")
+            .withExposedPorts(6379);
+
     @Test
     void testEmptyCoins() {
         Tile tile = new Tile(2, 3, TileType.Coin, 3, null);
@@ -20,7 +31,8 @@ class TestTile {
 
     @Test
     void testGetCaveCoin() {
-        Tile tile = new Tile(2, 3, TileType.Cave, 3, new ArrayList<>(List.of(new CaveCoin(1, CaveCoinType.Draw), new CaveCoin(2, CaveCoinType.Paddle))));
+        Tile tile = new Tile(2, 3, TileType.Cave, 3,
+                new ArrayList<>(List.of(new CaveCoin(1, CaveCoinType.Draw), new CaveCoin(2, CaveCoinType.Paddle))));
         tile.retreiveCoin();
         assertTrue(tile.hasCaveCoins());
         tile.retreiveCoin();
@@ -36,7 +48,8 @@ class TestTile {
 
     @Test
     void testGetCaveCoinCount() {
-        Tile tile = new Tile(2, 3, TileType.Cave, 3, new ArrayList<>(List.of(new CaveCoin(1, CaveCoinType.Draw), new CaveCoin(2, CaveCoinType.Paddle))));
+        Tile tile = new Tile(2, 3, TileType.Cave, 3,
+                new ArrayList<>(List.of(new CaveCoin(1, CaveCoinType.Draw), new CaveCoin(2, CaveCoinType.Paddle))));
         assertEquals(2, tile.getCaveCoinCount());
         tile.retreiveCoin();
         assertEquals(1, tile.getCaveCoinCount());
