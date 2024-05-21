5ee46f2db8d433a34060e2f887730502c2cef2e7
Mark Bruderer <markbruderer123@gmail.com>
2024-05-21 12:35:13 +0200

    test: add card power tests.

---
+++ src/test/java/org/utwente/market/CardTest.java
@@ -0,0 +1,48 @@
+package org.utwente.market;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.utwente.market.model.Card;
+import org.utwente.market.model.CardPowerException;
+import org.utwente.market.model.CardType;
+
+public class CardTest {
+  private Card card;
+
+  @BeforeEach
+  public void setup() {
+    this.card = new Card(CardType.Abenturerin);
+  }
+
+  @Test
+  public void testRemainingPower() {
+    assertEquals(CardType.Abenturerin.power.intValue(), card.remainingPower());
+  }
+
+  @Test
+  public void testRemovePower() {
+    try {
+      card.removePower(1);
+    } catch (CardPowerException e) {
+
+    }
+    assertEquals(1, card.remainingPower());
+  }
+
+  @Test
+  public void testRemoveMorePowerThanAvailable() {
+    assertThrows(CardPowerException.class, () -> {
+      card.removePower(4);
+    });
+  }
+
+  @Test
+  void testSettersCard() {
+    card.setCardType(CardType.Entdecker);
+    assertEquals();
+  }
+
+}
