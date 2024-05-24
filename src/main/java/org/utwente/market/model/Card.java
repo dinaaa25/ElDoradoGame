5b72bb8b7b18bdacc25873914ca11cab5816f5c6
Mark Bruderer <markbruderer123@gmail.com>
2024-05-24 18:17:31 +0200

    refactor: add override annotation.

--- src/main/java/org/utwente/market/model/Card.java
+++ src/main/java/org/utwente/market/model/Card.java
@@ -8,12 +8,12 @@ public class Card implements Resource {
     private CardType cardType;
     private int consumedPower;
 
-
     public Card(CardType cardType) {
         this.cardType = cardType;
         this.consumedPower = 0;
     }
 
+    @Override
     public PowerType getType() {
         return this.cardType.powerType;
     }
@@ -34,6 +34,7 @@ public class Card implements Resource {
         this.consumedPower += power;
     }
 
+    @Override
     public int getPower() {
         return remainingPower();
     }
