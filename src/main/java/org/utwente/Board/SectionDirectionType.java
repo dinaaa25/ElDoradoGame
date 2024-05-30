84199c815b28ada9f0d96b502ddb3ecb6fd1877c
Stijn Schuurman <stijn.schuurman@hotmail.com>
2024-05-30 18:54:32 +0200

    Refactor SectionDirectionType (minimize)

--- src/main/java/org/utwente/Board/SectionDirectionType.java
+++ src/main/java/org/utwente/Board/SectionDirectionType.java
@@ -3,75 +3,36 @@ package org.utwente.Board;
 public enum SectionDirectionType {
     FLAT_TOP {
         @Override
-        public SectionDirectionType.SectionDirection[] getSectionDirections() {
-            return SectionDirectionType.FlatTopSectionDirection.values();
+        public SectionDirection[] getSectionDirections() {
+            return FlatTopSectionDirection.values();
         }
     },
     POINTY_TOP {
         @Override
-        public SectionDirectionType.SectionDirection[] getSectionDirections() {
-            return SectionDirectionType.PointyTopSectionDirection.values();
+        public SectionDirection[] getSectionDirections() {
+            return PointyTopSectionDirection.values();
         }
     };
 
-    public abstract SectionDirectionType.SectionDirection[] getSectionDirections();
+    public abstract SectionDirection[] getSectionDirections();
 
-    public interface SectionDirection {
-        int getDq();
-        int getDr();
+    public interface SectionDirection {}
+
+    public enum FlatTopSectionDirection implements SectionDirection {
+        FT_NORTHEAST,
+        FT_EAST,
+        FT_SOUTHEAST,
+        FT_SOUTHWEST,
+        FT_WEST,
+        FT_NORTHWEST;
     }
 
-    public enum FlatTopSectionDirection implements SectionDirectionType.SectionDirection {
-        FT_NORTHEAST(1, -1),
-        FT_EAST(-1, 0),
-        FT_SOUTHEAST(1, 0),
-        FT_SOUTHWEST(-1, +1),
-        FT_WEST(-1, -1),
-        FT_NORTHWEST(-1, 0);
-
-        private final int dq;
-        private final int dr;
-
-        FlatTopSectionDirection(int dq, int dr) {
-            this.dq = dq;
-            this.dr = dr;
-        }
-
-        @Override
-        public int getDq() {
-            return dq;
-        }
-
-        @Override
-        public int getDr() {
-            return dr;
-        }
-    }
-
-    public enum PointyTopSectionDirection implements SectionDirectionType.SectionDirection {
-        PT_NORTH(0, -1),
-        PT_NORTHEAST(1, -1),
-        PT_SOUTHEAST(1, 0),
-        PT_SOUTH(0, 1),
-        PT_SOUTHWEST(-1, +1),
-        PT_NORTHWEST(-1, 0);
-
-        private final int dq;
-        private final int dr;
-
-        PointyTopSectionDirection(int dq, int dr) {
-            this.dq = dq;
-            this.dr = dr;
-        }
-
-        @Override
-        public int getDq() {
-            return dq;
-        }
-
-        @Override
-        public int getDr() {
-            return dr;
-        }
+    public enum PointyTopSectionDirection implements SectionDirection {
+        PT_NORTH,
+        PT_NORTHEAST,
+        PT_SOUTHEAST,
+        PT_SOUTH,
+        PT_SOUTHWEST,
+        PT_NORTHWEST;
     }
 }
