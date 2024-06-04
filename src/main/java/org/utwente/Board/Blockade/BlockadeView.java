2cbc56cbdfbc46ba914a0438438504d74c03a2f6
Stijn Schuurman <stijn.schuurman@hotmail.com>
2024-06-04 19:44:50 +0200

    Create GameConfig constant for InfoFontTyping

--- src/main/java/org/utwente/Board/Blockade/BlockadeView.java
+++ src/main/java/org/utwente/Board/Blockade/BlockadeView.java
@@ -3,6 +3,8 @@ package org.utwente.Board.Blockade;
 import java.awt.*;
 import java.awt.geom.Line2D;
 
+import static org.utwente.game.GameConfig.INFO_FONT;
+
 public class BlockadeView {
     public void draw(Graphics2D g2d, Blockade blockade, int yOffset) {
         int padding = 20;
@@ -13,7 +15,7 @@ public class BlockadeView {
         g2d.draw(new Line2D.Double(padding, padding + yOffset, padding + lineLength, padding + yOffset));
 
         g2d.setColor(Color.BLACK);
-        g2d.setFont(new Font("Arial", Font.BOLD, 12));
+        g2d.setFont(INFO_FONT);
         g2d.drawString("Points: " + blockade.getPoints(), padding + lineLength + 10, padding + yOffset - 10);
         g2d.drawString("Power: " + blockade.getPower(), padding + lineLength + 10, padding + yOffset);
         g2d.drawString("Removed: " + blockade.isRemoved(), padding + lineLength + 10, padding + yOffset + 10);
--- src/main/java/org/utwente/game/GameConfig.java
+++ src/main/java/org/utwente/game/GameConfig.java
@@ -7,7 +7,8 @@ import java.util.EnumMap;
 import java.util.Map;
 
 public class GameConfig {
-    public static final Font TILE_FONT = new Font("Arial", Font.BOLD, 8);;
+    public static final Font TILE_FONT = new Font("Arial", Font.BOLD, 8);
+    public static final Font INFO_FONT = new Font("Arial", Font.BOLD, 12);
     public static final int DEFAULT_BOARD_SIZE_X = 600;
     public static final int DEFAULT_BOARD_SIZE_Y = 800;
     public static final int PADDING = 50;
