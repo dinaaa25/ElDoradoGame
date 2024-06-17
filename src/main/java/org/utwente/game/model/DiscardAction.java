5f5ba68d6f1838d0ac5bf545681784420160e6e4
Mark Bruderer <markbruderer123@gmail.com>
2024-06-17 23:34:17 +0200

    implement discard action.

--- src/main/java/org/utwente/game/model/DiscardAction.java
+++ src/main/java/org/utwente/game/model/DiscardAction.java
@@ -1,5 +1,6 @@
 package org.utwente.game.model;
 
+import org.utwente.market.model.Card;
 import org.utwente.market.model.Resource;
 import org.utwente.player.model.Player;
 
@@ -11,12 +12,14 @@ public class DiscardAction extends Action {
 
     @Override
     public void execute() {
-
+        if (getResource() instanceof Card) {
+            player.discardCard((Card) getResource());
+        }
     }
 
     @Override
     public boolean validate() {
-        return true;
+        return getResource() instanceof Card;
     }
 
     @Override
@@ -24,4 +27,3 @@ public class DiscardAction extends Action {
 
     }
 }
-
