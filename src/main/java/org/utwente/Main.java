bc55fcccac28d6a4df80b18fc03abb34d9ccec33
Mark Bruderer <markbruderer123@gmail.com>
2024-06-20 21:57:29 +0200

    connect the config to Configuration class.

--- src/main/java/org/utwente/Main.java
+++ src/main/java/org/utwente/Main.java
@@ -2,23 +2,15 @@ package org.utwente;
 
 import lombok.Getter;
 
-import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.utwente.Board.Board;
-import org.utwente.Board.BoardView;
-import org.utwente.Board.Path;
 import org.utwente.game.model.Configuration;
-import org.utwente.game.model.Game;
 import org.utwente.game.controller.GameController;
 import org.utwente.game.view.GameCLI;
 import org.utwente.game.view.GameFrame;
 import org.utwente.game.view.GameGui;
 import org.utwente.game.view.GameView;
-import org.utwente.player.model.Player;
 
 import ch.qos.logback.classic.Level;
-
-import javax.swing.*;
 
 @Getter
 public class Main {
