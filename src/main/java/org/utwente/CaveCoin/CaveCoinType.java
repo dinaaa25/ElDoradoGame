6a5eb2e0eeddfb31feac5c34a116764fb3f8055e
Stijn Schuurman <stijn.schuurman@hotmail.com>
2024-06-21 11:57:06 +0200

    add PowerRange to CaveCoinType

--- src/main/java/org/utwente/CaveCoin/CaveCoinType.java
+++ src/main/java/org/utwente/CaveCoin/CaveCoinType.java
@@ -1,14 +1,26 @@
 package org.utwente.CaveCoin;
 
+import org.utwente.Tile.PowerRange;
+
 public enum CaveCoinType {
-    Machete,
-    Paddle,
-    Coin,
+    Machete(new PowerRange(1, 3)),
+    Paddle(new PowerRange(1, 2)),
+    Coin(new PowerRange(1, 2)),
     Draw,
     Remove,
     Replace,
     DontRemove,
     PassThrough,
     Adjacent,
-    Symbol,
+    Symbol;
+
+    private final PowerRange powerRange;
+
+    CaveCoinType(PowerRange powerRange) {
+        this.powerRange = powerRange;
+    }
+
+    CaveCoinType() {
+        this.powerRange = new PowerRange(1);
+    }
 }
