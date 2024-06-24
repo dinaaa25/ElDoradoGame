db6489aeb3afa61431a41ff5bb863b1b123891ac
Stijn Schuurman <stijn.schuurman@hotmail.com>
2024-06-24 12:20:28 +0200

    add removeUsedResources2 based on remainingPower field

--- src/main/java/org/utwente/game/controller/GameController.java
+++ src/main/java/org/utwente/game/controller/GameController.java
@@ -113,6 +113,12 @@ public class GameController {
         currentResources.removeIf(resource -> resource.getConsumedPower() != 0 && !isResourceSelected(resource));
     }
 
+    void removeUsedResources2(Event event) {
+        List<Card> currentResources = this.game.getCurrentPlayer().getPlayPile().getResources();
+        currentResources.removeIf(resource -> resource.remainingPower() <= 0);
+        this.game.getPhase().getSelectedResources().removeIf(resource -> resource.remainingPower() <= 0);
+    }
+
     void removeUsedResources(Event event) {
         List<Card> currentResources = this.game.getCurrentPlayer().getPlayPile().getResources();
         currentResources.removeIf(resource -> resource.getPower() <= 0);
