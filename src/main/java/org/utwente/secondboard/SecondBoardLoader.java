0328556401c357b7cb09c4420cdd2f1057b3868a
Stijn Schuurman <stijn.schuurman@hotmail.com>
2024-06-26 14:43:35 +0200

    add TileType conversion and other sections of the team4 board

--- src/main/java/org/utwente/secondboard/SecondBoardLoader.java
+++ src/main/java/org/utwente/secondboard/SecondBoardLoader.java
@@ -24,20 +24,43 @@ public class SecondBoardLoader {
         if (Objects.equals(color, new Color(0, 255, 0)))
         {
             return TileType.Machete;
+        } else if (Objects.equals(color, new Color(0, 100, 0))) {
+            return TileType.Start;
+        } else if (Objects.equals(color, Color.GRAY)) {
+            return TileType.Discard;
+        } else if (Objects.equals(color, Color.YELLOW)) {
+            return TileType.Coin;
+        } else if (Objects.equals(color, Color.CYAN)) {
+            return TileType.Paddle;
         }
         return TileType.Paddle;
     }
 
     public Board getConvertedBoard() {
         HexagonGameBoard board = new HexagonGameBoard(28, 28, 30,false);
-        List<Tile> tiles = new ArrayList<>();
+        List<Tile> tilesSection1 = new ArrayList<>();
+        List<Tile> tilesSection2 = new ArrayList<>();
+        List<Tile> tilesSection3 = new ArrayList<>();
+        List<Tile> tilesSection4 = new ArrayList<>();
         for (org.utwente.secondboard.boardPieces.Tile tile : board.boardPieces.get("Terrain_1").getTiles()) {
-            tiles.add(newTileToOGTile(tile));
+            tilesSection1.add(newTileToOGTile(tile));
         }
-        Section section = new Section(tiles, SectionType.A);
-        Board board2 = new Board(List.of(section), null, true, List.of());
+        for (org.utwente.secondboard.boardPieces.Tile tile : board.boardPieces.get("Terrain_2").getTiles()) {
+            tilesSection2.add(newTileToOGTile(tile));
+        }
+        for (org.utwente.secondboard.boardPieces.Tile tile : board.boardPieces.get("Terrain_3").getTiles()) {
+            tilesSection3.add(newTileToOGTile(tile));
+        }
+        for (org.utwente.secondboard.boardPieces.Tile tile : board.boardPieces.get("Terrain_4").getTiles()) {
+            tilesSection4.add(newTileToOGTile(tile));
+        }
+        Section section1 = new Section(tilesSection1, SectionType.A);
+        Section section2 = new Section(tilesSection2, SectionType.A);
+        Section section3 = new Section(tilesSection3, SectionType.A);
+        Section section4 = new Section(tilesSection4, SectionType.A);
+        Board board2 = new Board(List.of(section1, section2, section3, section4), null, true, List.of());
         System.out.println(board2);
-        return setStartingTiles(board2);
+        return board2;
     }
 
     public Board setStartingTiles(Board board) {
